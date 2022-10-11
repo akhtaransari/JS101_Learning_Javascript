817c8affe898290b14c37b20f104f0aae7a2aa42
Akhtar <akhtarcdgl@gmail.com>
2022-10-11 13:46:27 +0530

    update

---
+++ problem2.js
@@ -0,0 +1,6 @@
+let name = "Akhtar Ansari";
+console.log(name);
+name = "Jalaluddin Ansari";
+console.log(name);
+name = "Madina Begum";
+console.log(name);
---
+++ problem3.js
@@ -0,0 +1,4 @@
+let name = "Akhtar Ansari";
+let age = 27;
+console.log(name,age)
+console.log(typeof(name)),console.log(typeof(age));
